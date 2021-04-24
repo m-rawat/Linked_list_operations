8c7a1a34ad18427aca7498bb697c8d260167b33a
Mayank Rawat <52965332+m-rawat@users.noreply.github.com>
2021-04-24 19:46:55 +0530

    Update insertion at head.cpp

--- insertion at head.cpp
+++ insertion at head.cpp
@@ -1,3 +1,16 @@
+//create the node
+class node{
+public:
+    int data;
+    node *next;
+    node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+//insert at head
+
 void insertAtHead(node*&head,int d){
     if(head==NULL){
         head=new node(d);
