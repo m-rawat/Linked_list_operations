c57e383d7fd8c5499555947250632c00ab35ed4c
Mayank Rawat <52965332+m-rawat@users.noreply.github.com>
2021-04-25 12:38:34 +0530

    Create cycle detection in linked list.cpp

---
+++ cycle detection in linked list.cpp
@@ -0,0 +1,12 @@
+bool cycleDetection(node*head){
+    node*slow=head;
+    node*fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(fast==slow){
+            return true;
+        }
+    }
+    return false;
+}
